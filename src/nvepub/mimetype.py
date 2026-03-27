d30452faae141b11b494ecec61ea8d49abaf5c09
peter88213 <petert88213@gmail.com>
2026-03-27 11:45:01 +0100

    Minor refactoring.

--- src/nvepub/mimetype.py
+++ src/nvepub/mimetype.py
@@ -4,13 +4,9 @@ Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
-import datetime
-from string import Template
 
 
 class Mimetype:
 
-    _MIMETYPE = 'application/epub+zip'
-
     def write_mimetype(self):
-        self.write_file('mimetype', self._MIMETYPE)
+        self.write_file('mimetype', 'application/epub+zip')
